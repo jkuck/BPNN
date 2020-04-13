5ec7cb266738f66a1e54a9a42f62df15e4f16c0d
Hao Tang @ Stanford <tanghaosjtu@gmail.com>
2020-04-12 23:40:46 -0700

    Add search_bpnn_marginal.py

---
+++ batch/search_bpnn_marginal.py
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+import random, subprocess as sp
+
+PROCESS_NUM = 10
+
+jobs = [
+    'python learn_bp_map_marginal_spinglass.py %s %s %s %s -lr %f --alpha %f --layer_num %d'%(
+        dflag, aflag, mflag, cflag,
+        lr, damping, layer_num,
+    )
+    for dflag in ['--lr_decay_flag', '']
+    for aflag in ['--no_attractive_field', '']
+    for mflag in ['--model_map_flag', '']
+    for cflag in ['--classification_flag', '']
+    for lr in [1e-2, 1e-3, 1e-4, 1e-5]
+    for damping in [0.999, 0.99, 0.9, 0.7, 0.5, 0.3, 0.1, 0.01, 0.001]
+    for layer_num in [5, 10, 20]
+]
+
+random.shuffle(jobs)
+
+start_index = lambda i,n,d: min(int((n*i)/d), n)
+indexes = [start_index(i,len(jobs),PROCESS_NUM) for i in range(PROCESS_NUM+1)]
+for i in range(PROCESS_NUM):
+    fname = 'bpnn_marginal_jobs_%d.sh'%i
+    with open(fname, 'w') as f:
+        cur_jobs = jobs[indexes[i]:indexes[i+1]]
+        f.write('\n'.join(cur_jobs))
+    sp.call(['python', 'run.py', 'python', 'batch/run_scripts.py', fname])
