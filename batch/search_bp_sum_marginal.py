fec1d5ca56770ef40bc461d6e487e6f3356b712a
Hao Tang @ Stanford <tanghaosjtu@gmail.com>
2020-04-25 17:02:51 -0700

    Update batch-related files

---
+++ batch/search_bp_sum_marginal.py
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+import random, subprocess as sp
+
+PROCESS_NUM = 40
+
+jobs = []
+for updates in ['SEQFIX', 'SEQRND', 'SEQMAX', 'PARALL']:
+    for damping in [0.0, 0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 0.9, 0.99, 0.999]:
+        for mflag in ['--model_map_flag', '']:
+            for aflag in ['--attractive_flag', '']:
+                for miter in [10000, 5, 10, 20, 100, 200, 500,]:
+                    jobs.append('python bp_map_marginal_spinglass.py --data_map_flag %s %s --updates %s --damping %f --maxiter %d'%(aflag, mflag, updates, damping, miter))
+
+random.shuffle(jobs)
+
+start_index = lambda i,n,d: min(int((n*i)/d), n)
+indexes = [start_index(i,len(jobs),PROCESS_NUM) for i in range(PROCESS_NUM+1)]
+for i in range(PROCESS_NUM):
+    fname = 'bp_sum_marginal_jobs_%d.sh'%i
+    with open(fname, 'w') as f:
+        cur_jobs = jobs[indexes[i]:indexes[i+1]]
+        f.write('\n'.join(cur_jobs))
+    sp.call(['python', 'run_cpu.py', 'python', 'batch/run_scripts.py', fname])
--- batch/search_bpnn_sum_marginal.py
+++ batch/search_bpnn_sum_marginal.py
@@ -2,27 +2,22 @@
 
 import random, subprocess as sp
 
-PROCESS_NUM = 120
+PROCESS_NUM = 100
 
 jobs = [
-    'python learn_bp_map_marginal_spinglass.py %s %s %s %s %s %s %s %s -lr %f --alpha %f --layer_num %d --one_hot_ratio %s'%(
-        dflag, aflag, mflag, cflag, sflag, bflag, tflag, lname,
-        10**(random.random()*5-7), damping, layer_num, one_hot_ratio
+    'python learn_bp_map_marginal_spinglass.py --data_map_flag %s %s %s %s %s %s -lr %f --alpha %f --layer_num %d'%(
+        dflag, aflag, mflag, cflag, sflag, bflag,
+        10**(random.random()*5-7), damping, layer_num
     )
     for dflag in ['--lr_decay_flag', '']
-    # for aflag in ['--no_attractive_flag', '']
-    for aflag in ['--no_attractive_flag']
+    for aflag in ['--no_attractive_flag', '']
     for mflag in ['--model_map_flag', '']
     for cflag in ['--classification_flag']
-    for lname in ['--loss_name one_hot_cross_entropy', '']
     for sflag in ['--share_weights_flag', '']
     for bflag in ['--bethe_flag', '']
-    # for tflag in ['--no_training_flag', '']
-    for tflag in ['']
     for _ in range(10) #slots for learing rate
     for damping in [0.999, 0.99, 0.9, 0.7, 0.5, 0.3, 0.1, 0.01, 0.001]
     for layer_num in [5, 10, 20, 30, 50, 100, 200, 500]
-    for one_hot_ratio in [0, 0.33, 0.67, 1]
 ]
 
 random.shuffle(jobs)
@@ -30,7 +25,7 @@ random.shuffle(jobs)
 start_index = lambda i,n,d: min(int((n*i)/d), n)
 indexes = [start_index(i,len(jobs),PROCESS_NUM) for i in range(PROCESS_NUM+1)]
 for i in range(PROCESS_NUM):
-    fname = 'bpnn_marginal_jobs_%d.sh'%i
+    fname = 'bpnn_sum_marginal_jobs_%d.sh'%i
     with open(fname, 'w') as f:
         cur_jobs = jobs[indexes[i]:indexes[i+1]]
         f.write('\n'.join(cur_jobs))
